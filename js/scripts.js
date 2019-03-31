4a3c3392643e88a248bf15a8acf3bfde215864e2
Zach Weintraub <zachweintraub@gmail.com>
2019-03-31 10:43:40 -0700

    add win alert, bad guesses display, change system for displaying blanks and correct letters, and add delay on lose alert to first display final photo

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,50 @@
+var words = ['pizza','spaghetti', 'gnocchi', 'taco', 'watermelon', 'burrito', 'lasagna', 'risotto', 'quesadilla', 'teriyaki', 'sushi', 'macaroni', 'dragonfruit', 'falafel', 'hummus', 'curry', 'cheeseburger', 'meatloaf', 'sundae', 'tiramisu', 'croissant', 'muffin', 'ceviche', 'fondue', 'bratwurst', 'sausauge', 'omlette', 'dumpling', 'pierogi']
+var phrase = words[parseInt(Math.random()*words.length)];
+console.log(phrase);
+var phraseArray = phrase.split('');
+var guessedLetter;
+var phraseLength = phraseArray.length;
+var picNumber = 1;
+var correctLetters = 0;
+var wordInProgress = '';
+var badLetters = '';
+
 $(function(){
-  var words = ['pizza','invader', 'jacket', 'computer', 'watermelon', 'epicodus', 'lasagna', 'beer', 'vodka', 'beautiful', 'tetris', 'halo', 'scrabble']
-  var phrase = words[parseInt(Math.random()*words.length)];
-  console.log(phrase);
-  var phraseArray = phrase.split('');
-  var guessedLetter;
-  var phraseLength = phraseArray.length;
-  var picNumber = 1;
-  $('#word-length').append('<p>this word has ' + phraseArray.length + ' letters.</p>')
+  $('#word-length').append('<p>this word has ' + phraseLength + ' letters.</p>')
   for(var i = 0; i < phraseLength; i++) {
-    $('#answer-blanks').append('<p id="letter' + i + '">' + phraseArray[i]+'</p>');
+    wordInProgress = wordInProgress.concat('_');
+    $('#answer').text(wordInProgress);
   }
   $('button').click(function(){
-    guessedLetter = $('#guess-input').val().toLowerCase();
+    guessedLetter = $('#guess-input').val().toLowerCase().charAt(0);
     $('#guess-input').val('');
     if(phrase.includes(guessedLetter)) {
       for(var i = 0; i < phraseLength; i++) {
       if(guessedLetter == phraseArray[i]) {
-        $('#letter'+i).addClass('letter');
+        console.log('got it right');
+        wordInProgress = wordInProgress.substr(0, i) + guessedLetter + wordInProgress.substr(i + 1);
+        $('#answer').text(wordInProgress);
+        correctLetters++;
       }
-    }} else {
-      if(picNumber==6){
-        $('#'+picNumber).remove();
-        picNumber++;
-        $('#images').append('<img id="' + picNumber + '" src="img/hangman' + picNumber +'.jpg">');
-          if(confirm('YOU LOSE! the answer was "' + phrase + '". play again?')){
-            location.reload();
-          }
       }
+      if(correctLetters == phraseLength) {
+        setTimeout(function(){if(confirm('YOU WIN! play again?')){
+          location.reload();
+        }}, 500);
+      }
+    } else {
+
       $('#'+picNumber).remove();
       picNumber++;
       $('#images').append('<img id="' + picNumber + '" src="img/hangman' + picNumber +'.jpg">');
-
-
-
-
-  }
-
+      badLetters = badLetters.concat(guessedLetter);
+      $('#bad-letters').text(badLetters);
+      if(picNumber==7){
+        setTimeout(function(){
+          if(confirm('YOU LOSE! The answer was "' + phrase + '" - wanna try again?')){
+            location.reload();
+          }
+        }, 1000)}
+      }
+    });
   });
-
-
-
-
-
-
-
-});
